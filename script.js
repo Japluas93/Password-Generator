d11015d803e2b04df668e5547671d5095b782082
Julian Pluas <julianpluas@gmail>
2020-10-24 19:23:36 -0700

    First function was not rendering correctly. Realized that my validation statements were not named correctly (forgot to change them after I copy and pasted them). Did a test, was able to generate a password

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ var validNumber = "0123456789"
 var validSpecial = "!@#$%^&*()-=+?><"
 
 // WHEN I click the button to generate a password
-generateBtn.addEventListener("click", function() {
+generateBtn.addEventListener("click", function(){
   var passwordLength = 0
-  while(passwordLength < 8 || passwordLength >128){
+  while(passwordLength < 7 || passwordLength > 128) {
     var passwordLength = prompt("Your password must contain at least 8 characters, and it cannot exceed 128 characters");
   }
-  console.log(passwordLength);upperCase
-
+  console.log(passwordLength);
     var upperCase = false
     var lowerCase = false
     var numberChar = false
@@ -24,45 +23,25 @@ generateBtn.addEventListener("click", function() {
     var numberChar = confirm("Would you like to include any numeric characters in your password?");
     var specialChar = confirm("Would you like to include any special characters in your password?");
   }
+  // data validation 
     var validchar = []
     if (upperCase) {
-      validchar +=validUpper 
+      validchar += validUpper 
     }
     if (lowerCase) {
-      validchar +=validUpper 
+      validchar += validLower 
     }
     if (numberChar) {
-      validchar +=validUpper 
+      validchar += validNumber 
     }
     if (specialChar) {
-      validchar +=validUpper 
+      validchar += validSpecial 
     }
   console.log(validchar)
-  })
-  // WHEN prompted for password criteria
-  // THEN I select which criteria to include in the password
-
-  // // WHEN prompted for the length of the password
-  // var passwordLength = prompt["Password must be between 8 and 128 characters"];
-  // // THEN I choose a length of at least 8 characters and no more than 128 characters
-  // var item = items[Math.floor(Math.random()*items.length)];
-
-  // WHEN prompted for character types to include in the password
-  // THEN I choose lowercase, uppercase, numeric, and/or special characters
-//   // WHEN I answer each prompt
-//   // THEN my input should be validated and at least one character type should be selected
-//   else
-// // WHEN all prompts are answered
-// // THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
+  for (let i=1; i <= passwordLength; i++){
+  var index = Math.floor(Math.random()*validchar.length);
+  password += validchar[index]
+}
+var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+})
